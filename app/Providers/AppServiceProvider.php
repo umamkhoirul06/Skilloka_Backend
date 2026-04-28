c07e3b452707880b52c7e04f10c819b0d002b325
khoirulumam06 <khoerulumam163@gmail.com>
2026-04-28 19:59:20 +0700

    fix: memaksa tailwind dan vite menggunakan https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Memaksa semua link CSS/JS (Tailwind) menjadi HTTPS
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        } else {
+            // Khusus VPS/Production ini akan langsung memaksa HTTPS
+            URL::forceScheme('https');
+        }
     }
 }
